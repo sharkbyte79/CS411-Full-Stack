b8f28231641450d06ccfe1bcc268fa80326de74c
Wes1eyCh0i <wesleyc0830lc@gmail.com>
2024-03-31 19:04:33 -0400

    add authorization and create playlist function for the frontend

---
+++ src/globals.ts
@@ -0,0 +1,6 @@
+export const CLIENT_ID = "6b4f68c182c94f7bb2d0f6a27578a26b"
+export const CLIENT_SECRET = "047ea3468701405480bbf9a58424e955"
+export const REDIRECT_URI = "http://localhost:3000"
+export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+export const RESPONSE_TYPE = "token"
+export const SCOPE = "playlist-modify-private"
---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface IUserInformation {
+  country: string;
+  display_name: string;
+  email: string;
+  external_urls: string;
+  href: string;
+  id: string;
+  images: {
+            url: string;
+            height: string;
+            width: string
+          } []
+  type: string;
+  uri: string
+}
