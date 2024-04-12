2a825aa7ed777a2dfd1366570be77ca47377a5bf
Layne Pierce <ppierce273@gmail.com>
2024-04-12 09:57:11 -0400

    Add mongoose schema for a Spotify playlist

---
+++ backend/models/Playlist.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const playlistSchema = new mongoose.Schema({
+  // NOTE: Spotify api returns playlists in the form of a
+  // JSON object, but breaking that object up for the schema
+  // may be preferable.
+  playlist: { type: Object, required: true, unique: true },
+  // cover image pulled from the other api
+  cover: { type: Object, required: false, unique: true },
+});
+
+const Playlist = mongoose.model("Playlist", playlistSchema);
+
+module.exports = Playlist;
