c08c43c5411c8acdf9b26609812548df0e0b7c36
Layne Pierce <ppierce273@gmail.com>
2024-04-21 19:07:46 -0400

    Add cover route, logic for getting cat image from api and sending to database

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,37 @@
 const express = require("express");
-const cors = require("cors");
-//const routes = require('./routes');
+//const cors = require("cors");
 
 const app = express();
-app.use(cors());
 
 // Fetch preferred port, Spotify and Mongo keys, etc.
 require("dotenv").config();
 const port = process.env.PORT || 3000; // Use preferred port or default to 3000
+const mongodb_uri = process.env.MONGODB_URI;
+
 
 // Include routes
-const loginRoute = require("./routes/login");
-const makePlaylistRoute = require("./routes/make_playlist");
+const login = require("./routes/login");
+const cover = require("./routes/cover");
 // TODO: MORE ROUTES HERE
 
-// const mongoose = require("mongoose")
-// mongoose
-//   .connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("Connected to MongoDB"))
-//   .catch((err) => console.error("Could not connect to MongoDB:", err));
+app.use("/", login);
+app.use("/", cover)
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(mongodb_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("[server]: Connected to MongoDB"))
+  .catch((err) => console.error("[server]: Could not connect to MongoDB:", err));
 
 app.use(express.json());
-app.use("/", loginRoute);
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`[server]: Server running at http://localhost:${port}`);
 });
