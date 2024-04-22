fb9acf3d714f9a39b1cc9bc8797a379425caf736
Layne Pierce <ppierce273@gmail.com>
2024-04-21 23:52:16 -0400

    Add api key to CatAPI request string

--- backend/routes/cover.js
+++ backend/routes/cover.js
@@ -1,24 +1,33 @@
 const express = require('express');
+const querystring = require('querystring');
 const axios = require('axios');
 const Cover = require('../models/Cover');
 
 const router = express.Router();
 
+require('dotenv').config({ path: '../.env' });
+const cat_api_key = process.env.CAT_API_KEY;
+
 router.get('/cover', async (req, res) => {
     try {
         // Get a single image from TheCatApi.
-        // The api supports fetching an array of JSON objects with multiple entries,
-        // but for a single playlist we only need one.
-
-        let response; // response may need to be reassigned if its url field points to a gif, so let it be mutable
+        // The api supports fetching an array of JSON objects with multiple entries, but for a single playlist we only need one.
+        var response; // response may need to be reassigned if its url field points to a gif, so let it be mutable
         let isGIF = true;
+
         while (isGIF) {
             response = await axios.get(
-                'https://api.thecatapi.com/v1/images/search'
+                'https://api.thecatapi.com/v1/images/search?' +
+                    querystring.stringify({
+                        api_key: cat_api_key,
+                    })
             );
-
             urlParts = response.data[0].url.split('.');
             fileExt = urlParts[3];
+            console.log(
+                '[server]: Request contained url with extension:',
+                fileExt
+            );
             // Stop making requests if the return isn't a gif
             if (fileExt != 'gif') {
                 isGIF = false;
