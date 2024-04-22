996b609d19839c82cd0a99913cc2691254971488
Layne Pierce <ppierce273@gmail.com>
2024-04-21 20:00:43 -0400

    Fix typo on String, Number types in Cover schema

--- backend/models/Cover.js
+++ backend/models/Cover.js
@@ -1,12 +1,29 @@
 const mongoose = require("mongoose");
 
-const coverScheme = new mongoose.Schema({
-  id: { type: string, required: true, unique: true },
-  url: { type: string, required: true, unique: true },
-  width: { type: number, required: false, unique: false },
-  height: { type: number, required: false, unique: false },
+const coverSchema = new mongoose.Schema({
+  // The Cat API returns a JSON array that contains the following fields:
+  id: {
+    type: String, 
+    required: true, 
+    unique: true,
+  },
+  url: {
+    type: String, 
+    required: true, 
+    unique: true,
+  },
+  width: { 
+    type: Number,
+    required: false,
+    unique: false,
+  },
+  height: { 
+    type: Number,
+    required: false,
+    unique: false
+  },
 });
 
-const Cover = mongoose.model("Cover", coverScheme);
+const Cover = mongoose.model("Cover", coverSchema);
 
 module.exports = Cover;
