9eaddf907931740bc1de337d28ece559b47a3cf3
Wes1eyCh0i <wesleyc0830lc@gmail.com>
2024-05-05 13:26:24 -0400

    fix frontend and playlist

--- backend/routes/playlist.js
+++ backend/routes/playlist.js
@@ -4,8 +4,30 @@ const mongoose = require('mongoose');
 const Playlist = require('../models/Playlist');
 const Token = require('../models/Token');
 const Cover = require('../models/Cover');
+const querystring = require('querystring');
 
 const router = express.Router();
+
+function getRandomSearch() {
+    // A list of all characters that can be chosen.
+    const characters = 'abcdefghijklmnopqrstuvwxyz';
+
+    // Gets a random character from the characters string.
+    const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length));
+    let randomSearch = '';
+
+    // Places the wildcard character at the beginning, or both beginning and end, randomly.
+    switch (Math.round(Math.random())) {
+        case 0:
+            randomSearch = randomCharacter + '%';
+            break;
+        case 1:
+            randomSearch = '%' + randomCharacter + '%';
+            break;
+    }
+
+    return randomSearch;
+}
 
 router.get('/playlist', async (req, res) => {
     try {
@@ -31,10 +53,41 @@ router.get('/playlist', async (req, res) => {
             }
         });
 
+        console.log(playlistResponse.data)
+
         const playlist = new Playlist({ playlist: playlistResponse.data });
         await playlist.save();
 
-        res.status(200).json({ playlist: playlistResponse.data });
+        const SongResponse = await axios.get(`https://api.spotify.com/v1/search?` +
+            querystring.stringify({
+                q: getRandomSearch(),
+                type: "track",
+                limit: 10
+            }), {
+            headers: {
+                'Authorization': `Bearer ${token[0].access_token}`, // Include the access token in the headers
+            }
+        });
+
+        const songURI = []
+
+        for (const Song of SongResponse.data.tracks.items) {
+            songURI.push(Song.uri)
+        }
+
+        const AddingResponse = await axios.post(`https://api.spotify.com/v1/playlists/${playlistResponse.data.id}/tracks`, {
+            uris: songURI,
+            position: 0
+        }, {
+            headers: {
+                'Authorization': `Bearer ${token[0].access_token}`, // Include the access token in the headers
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log(AddingResponse.data)
+
+        res.status(200).json(true);
         // Save the playlist to MongoDB
         // const playlist = new Playlist({ playlist: playlistResponse.data });
         // await playlist.save();
--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,9 @@ import './App.css';
 
 const App = () => {
   /* UseStae Declaration */
-  const [token, setToken] = useState<string>("");
   const [userId, setUserId] = useState<string>("");
-  const [user, setUser] = useState<IUserInformation | null>(null);
-  const [songIDs, setSongIDs] = useState<string[]>(["0kdqcbwei4MDWFEX5f33yG", "44L2bY93uD65CEHWbozpx4", "5TiGe89LNDwridBLbLBGgR", "22VHOlVYBqytsrAqV8yXBK", "0ZpHuEhi1CvOJgrqOSy8mv", "60bAuEmJQfzeDV1B84H4xY"]);
-  const [songs, setSongs] = useState<ISongInformation[]>([]);
-  const [playlist, setPlaylist] = useState<IPlaylist | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [hasPlayed, setHasPlayed] = useState<boolean>(false);
 
   /* Logout the user and delete */
   const logout = () => {
@@ -26,8 +22,6 @@ const App = () => {
     try {
       const res = await fetch('login');
       const data = await res.json()
-      // const url = `${CONST.AUTH_ENDPOINT}?client_id=${CONST.CLIENT_ID}&redirect_uri=${CONST.REDIRECT_URI}&response_type=${CONST.RESPONSE_TYPE}&scope=${CONST.SCOPE}`;
-      // window.location.href = url;
       console.log(data)
       window.location.href = data
     } catch (error) {
@@ -37,99 +31,15 @@ const App = () => {
 
   /* Functions that use Sportify API */
 
-  const fetchUserInfomation = async () => {
+  const Playlist = async () => {
     try {
-      const res = await fetch(`https://api.spotify.com/v1/me`, {
-        method: "GET",
-        headers: {
-          "Authorization": `Bearer ${token}`
-        }
+      const res = await fetch(`playlist/?user_id=` + userId, {
+        method: "GET"
       });
       const json = await res.json();
-      console.log(json);
-      setUser(json);
+      setHasPlayed(json)
     } catch (error) {
       console.error("Error fetching user information:", error);
-    }
-  };
-
-  const createPlaylist = async (user_id: string) => {
-    try {
-      const data = {
-        "name": "New Playlist",
-        "description": "New playlist description",
-        "public": false
-      };
-      const res = await fetch(`https://api.spotify.com/v1/users/${user_id}/playlists`, {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${token}`,
-          "Content-Type": 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-      const json = await res.json();
-      console.log(json);
-      setPlaylist(json);
-    } catch (error) {
-      console.error("Error creating a playlist:", error);
-    }
-  };
-
-  const addItems = async (playlist_id: string) => {
-    let uris = [];
-    for (const song of songs) {
-      uris.push(song.uri);
-    }
-
-    try {
-      const data = {
-        "uris": uris,
-        "position": 0
-      };
-      const res = await fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks`, {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${token}`,
-          "Content-Type": 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-      const json = await res.json();
-      console.log(json);
-    } catch (error) {
-      console.error("Error adding items:", error);
-    }
-  };
-
-  async function fetchSingleSongInformation(id: string) {
-    try {
-      const res = await fetch(`https://api.spotify.com/v1/tracks/${id}`, {
-        method: "GET",
-        headers: {
-          "Authorization": `Bearer ${token}`
-        }
-      });
-      const json = await res.json();
-      console.log(json);
-      return json;
-    } catch (error) {
-      console.error("Error fetching user information:", error);
-    }
-  };
-
-  const fetchSongsInformation = async (ids: string[]) => {
-    try {
-      let json = [];
-      for (const id of ids) {
-        const info = await fetchSingleSongInformation(id)
-        json.push(info);
-      }
-
-      console.log(json);
-      setSongs(json);
-    } catch (error) {
-      setSongs([]);
     }
   };
 
@@ -138,26 +48,12 @@ const App = () => {
 
     setIsLoading(true);
     try {
-      await fetchSongsInformation(songIDs);
+      await Playlist();
       setIsLoading(false);
     } catch (error) {
       console.error('Error fetching songs information:', error);
     }
   };
-
-  const createClick = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    event.preventDefault();
-
-    try {
-      if (user == null) {
-        throw new Error("User is null")
-      }
-      await createPlaylist(user.id);
-    } catch (error) {
-      console.error('Error fetching songs information:', error);
-    }
-  };
-
   /* UseEffect */
 
   /* Try to get the token when the website starts */
@@ -175,53 +71,9 @@ const App = () => {
   }, [])
 
   /* Fetch the user information when we have the token */
-  useEffect(() => {
-    if (token !== "" && token) {
-      fetchUserInfomation();
-    }
-  }, [token])
-
-  useEffect(() => {
-    if (playlist !== null && playlist) {
-      addItems(playlist.id);
-    }
-  }, [playlist]);
 
   /* Helper functions */
-
-  /* Helper function of displayImage*/
-  function chunkArray(songs: ISongInformation[], chunkSize: number) {
-    const chunks = [];
-    for (let i = 0; i < songs.length; i += chunkSize) {
-      chunks.push(songs.slice(i, i + chunkSize));
-    }
-    return chunks;
-  };
-
-  /* Generate the correct html for image displayment */
-  function displayImage(songs: ISongInformation[]) {
-    const chunkedUrls = chunkArray(songs, 3);
-
-    return (
-      <div className="container">
-        {chunkedUrls.map((group, index) => (
-          <div key={index} className="group">
-            {group.map((song, i) => (
-              <div key={i} className="songContainer">
-                <a href={song.external_urls.spotify} target="_blank" rel="noopener noreferrer">
-                  <img src={song.album.images[1].url} alt={`Image ${i + 1}`} style={{ marginRight: i !== group.length - 1 ? '50px' : '0', cursor: 'pointer' }} />
-                </a>
-                <div className="songName">{song.name}</div>
-                <div className="artistName">{song.artists[0].name}</div>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    );
-  };
-
-  if (songs.length === 0) {
+  if (hasPlayed) {
     return (
       <div className="App">
         <header className="App-header">
@@ -234,9 +86,9 @@ const App = () => {
               <button onClick={logout}>Logout</button>
               : <button onClick={spotifyLogin}>Login To Start</button>}
             {userId ?
-              <button onClick={playClick}>Let's Play</button>
+              <button onClick={playClick}>Play again</button>
               : <></>}
-            <p className="description">Create a Spotify playlist based on cute photos of cats!</p>
+            <p className="description">Playlist has been created!</p>
           </div>) : (<Circles />)}
         </header>
       </div>
@@ -246,24 +98,34 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> About </h1>
-        {displayImage(songs)}
-        <button onClick={createClick}>Create Playlist</button>
+        {!isLoading ? (<h1> Spotify Arcade <BsSpotify /></h1>)
+          : <h1> Generating</h1>
+        }
+
+        {!isLoading ? (<div className="Button">
+          {userId ?
+            <button onClick={logout}>Logout</button>
+            : <button onClick={spotifyLogin}>Login To Start</button>}
+          {userId ?
+            <button onClick={playClick}>Let's Play</button>
+            : <></>}
+          <p className="description">Create a Spotify playlist based on cute photos of cats!</p>
+        </div>) : (<Circles />)}
       </header>
     </div>
+  )
 
 
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <h1>Spotify React</h1>
-    //     {!token ?
-    //       <a href={`${CONST.AUTH_ENDPOINT}?client_id=${CONST.CLIENT_ID}&redirect_uri=${CONST.REDIRECT_URI}&response_type=${CONST.RESPONSE_TYPE}&scope=${CONST.SCOPE}`}>
-    //         Login to Play</a>
-    //       : <button onClick={logout}>Logout</button>}
-    //   </header>
-    // </div>
-  );
+  // <div className="App">
+  //   <header className="App-header">
+  //     <h1>Spotify React</h1>
+  //     {!token ?
+  //       <a href={`${CONST.AUTH_ENDPOINT}?client_id=${CONST.CLIENT_ID}&redirect_uri=${CONST.REDIRECT_URI}&response_type=${CONST.RESPONSE_TYPE}&scope=${CONST.SCOPE}`}>
+  //         Login to Play</a>
+  //       : <button onClick={logout}>Logout</button>}
+  //   </header>
+  // </div>
 }
 
 export default App;
